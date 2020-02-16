1ccbeef4db2dd21fd9bdff568e91eeec0351f75a
viveksenthilsj <viveksmaster@gmail.com>
2020-02-16 22:31:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-$('#galaxyIframe').contents().find('li.ACTION-export.TARGET-CSV').click()
+setTimeout(function() {   //calls click event after a certain time
+   $('#galaxyIframe').contents().find('li.ACTION-export.TARGET-CSV').click()
+}, 15000);
